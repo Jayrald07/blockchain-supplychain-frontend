7e6147322e7f45e0a6ebb13f1b10397e034fc20b
clintashleyy <91358701+clintashleyy@users.noreply.github.com>
2023-01-25 02:39:24 +0800

    added dashboard card

---
+++ src/Components/Dashboard/dashboard-card.tsx
@@ -0,0 +1,21 @@
+type Button = {
+    label1: string;
+    label2: string;
+    icon?: React.ReactNode;
+  };
+
+export default ({ label1, label2, icon }: Button) => {
+  return (
+        <div className="dashboard-container">
+            <div className="dashboard-icon">{icon}</div>
+                <div className="dashboard-content">
+                    <div className="small-text">
+                        <label>{label1}</label>
+                    </div>
+                    <div className="big-text">
+                        <label>{label2}</label>
+                    </div>
+            </div>
+        </div>
+  );
+};
