0b356d154cffdc9ac4b43d35eee31ca0467569a7
clintashleyy <91358701+clintashleyy@users.noreply.github.com>
2023-01-28 15:22:50 +0800

    register added (unfinished)

---
+++ src/Components/Register/register.index.tsx
@@ -0,0 +1,72 @@
+import Input from "./register-input.index";
+import "./register-card.index.css";
+
+
+export default () => {
+  return (
+    <form className="bsc-asset">
+      <h2>Register</h2>
+      <section className="bsc-input">
+        <Input
+          type="text"
+          label="Organization name"
+          placeholder="Organization name"
+        />
+        <Input
+          type="text"
+          label="Type of organization"
+          placeholder="Type of organization"
+        />
+      </section>
+      <div className="bsc-input">
+        <Input
+            type="text"
+            label="Address"
+            placeholder="Address"
+        />
+      </div>
+      <section className="bsc-input">
+        <Input
+          type="text"
+          label="Username"
+          placeholder="Username"
+        />
+        <Input
+          type="text"
+          label="Password"
+          placeholder="Password"
+        />
+        <Input
+          type="text"
+          label="Contact number"
+          placeholder="+63xxxxxxxxxx"
+        />
+      </section>
+      <h2>Node Creation</h2>
+      <section className="bsc-input">
+        <Input
+            type="text"
+            label="Type of node"
+            placeholder="Type of node"
+        />
+      </section>
+      <section className="bsc-input">
+        <Input
+          type="text"
+          label="Private key"
+          placeholder="Private key"
+        />
+        <Input
+          type="text"
+          label="Connection certificate"
+          placeholder="Connection certificate"
+        />
+        <Input
+          type="text"
+          label="Unique ID"
+          placeholder="Unique ID"
+        />
+      </section>
+    </form>
+  );
+};
