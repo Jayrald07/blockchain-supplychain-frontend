7a1122222d1d62f2a5cf532cf51c3bd9909dd523
Jayrald07 <geraldempino@gmail.com>
2023-01-31 18:31:02 +0800

    update qr scanning camera

--- src/Components/ScanQR/scanqr.index.tsx
+++ src/Components/ScanQR/scanqr.index.tsx
@@ -1,33 +1,47 @@
 import { Html5Qrcode } from "html5-qrcode";
-import { useEffect, useRef } from "react";
+import { CameraDevice } from "html5-qrcode/esm/camera/core";
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 import ButtonIndex from "../Button/button.index";
 import "./scanqr.index.css";
 
 export default () => {
   let html5qr: any = useRef(null);
+  const [camera, setCamera] = useState([]);
+
+  const handleSetCamera = async (event: any) => {
+    console.log(html5qr.current.isScanning);
+    if (html5qr.current.isScanning) await html5qr.current.stop();
+
+    html5qr.current.start(
+      event.target.value,
+      {
+        fps: 10,
+        disableFlip: false,
+        qrbox: { width: 200, height: 200 },
+      },
+      (text: any, result: any) => console.log({ text, result }),
+      (error: any) => {}
+    );
+  };
 
   useEffect(() => {
-    html5qr.current = new Html5Qrcode("qr-scan");
-    Html5Qrcode.getCameras().then((devices) => {
-      var camera = devices[0].id;
-      console.log({ camera: devices[0].label });
-
-      html5qr.current.start(
-        camera,
-        {
-          fps: 10,
-          disableFlip: false,
-          qrbox: { width: 200, height: 200 },
-        },
-        (text: any, result: any) => console.log({ text, result }),
-        (error: any) => {}
-      );
+    Html5Qrcode.getCameras().then((devices: any) => {
+      setCamera(devices);
     });
+
+    html5qr.current = new Html5Qrcode("qr-scan");
   }, []);
 
   return (
     <section className="qr-scan-container">
       <div id="qr-scan"></div>
+      <select onChange={handleSetCamera}>
+        {camera.map((item: CameraDevice) => (
+          <option value={item.id} key={item.id}>
+            {item.label}
+          </option>
+        ))}
+      </select>
     </section>
   );
 };
