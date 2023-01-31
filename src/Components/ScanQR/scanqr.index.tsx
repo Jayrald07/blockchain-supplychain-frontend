0624fadef15e1e97a7ca765afd39431cc3c5923d
Jayrald07 <geraldempino@gmail.com>
2023-01-31 19:21:10 +0800

    update camera to use back camera

--- src/Components/ScanQR/scanqr.index.tsx
+++ src/Components/ScanQR/scanqr.index.tsx
@@ -6,14 +6,13 @@ import "./scanqr.index.css";
 
 export default () => {
   let html5qr: any = useRef(null);
-  const [camera, setCamera] = useState([]);
 
-  const handleSetCamera = async (event: any) => {
-    console.log(html5qr.current.isScanning);
-    if (html5qr.current.isScanning) await html5qr.current.stop();
-
+  useEffect(() => {
+    html5qr.current = new Html5Qrcode("qr-scan");
     html5qr.current.start(
-      event.target.value,
+      {
+        facingMode: "environment",
+      },
       {
         fps: 10,
         disableFlip: false,
@@ -22,31 +21,11 @@ export default () => {
       (text: any, result: any) => console.log({ text, result }),
       (error: any) => {}
     );
-  };
-
-  useEffect(() => {
-    Html5Qrcode.getCameras().then((devices: any) => {
-      setCamera(devices);
-    });
-
-    (async function () {
-      html5qr.current = new Html5Qrcode("qr-scan");
-      await html5qr.current.applyVideoConstraints({
-        facingMode: "environment",
-      });
-    })();
   }, []);
 
   return (
     <section className="qr-scan-container">
       <div id="qr-scan"></div>
-      <select onChange={handleSetCamera}>
-        {camera.map((item: CameraDevice) => (
-          <option value={item.id} key={item.id}>
-            {item.label}
-          </option>
-        ))}
-      </select>
     </section>
   );
 };
